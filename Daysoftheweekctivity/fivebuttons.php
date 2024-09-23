612e13d85ba5da0ed1abd04a199b44f99c894326
JohnEmmanuel Sabuero <sabuerojohnemmanuel53@gmail.com>
2024-09-23 22:26:40 +0800

    first commit

---
+++ Daysoftheweekctivity/fivebuttons.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="monday.php" method="post">
+    <input type="submit" value="Monday" name="M">
+    </form>
+    <form action="tuesday.php" method="post">
+    <input type="submit" value="Tuesday" name="T">
+    </form>
+    <form action="wednesday.php" method="post">
+    <input type="submit" value="Wednesday" name="W">
+    </form>
+    <form action="thursday.php" method="post">
+    <input type="submit" value="Thursday" name="Th">
+    </form>
+    <form action="friday.php" method="post">
+    <input type="submit" value="Friday" name="F">
+    </form>
+
+    
+</body>
+</html>
